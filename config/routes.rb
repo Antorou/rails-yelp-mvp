734bd70d1aea19e5718599cc9a7e2d96d9f3b89a
Antoine Rousselot <rousselotantoine7@gmail.com>
2024-11-13 11:58:14 +0100

    processing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    resources :restaurants do
+      resources :reviews, only: [:index, :new, :create]  # Nested routes for reviews under restaurants
+    end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+    resources :reviews, only: [:show, :edit, :update, :destroy]  # Routes for individual review
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
